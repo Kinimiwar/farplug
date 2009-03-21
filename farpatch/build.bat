2f5ee5eb8320f0378c725350003337e7072b3df0
farplug <farplug@localhost>
2009-03-21 08:58:26 +0000

    +build script

--- farpatch/build.bat
+++
@@ -1,7 +0,0 @@
-unicode_far\tools\m4 -P -I unicode_far installer\installer.mak.m4 > installer.mak
-call setenv /x86
-nmake -f installer.mak
-@if errorlevel 1 goto end
-call setenv /x64
-nmake -f installer.mak
-:end
