45626dd5c4b4538b0c50f206b6df6537960f3c96
farplug <farplug@localhost>
2009-12-14 22:43:51 +0000

    x64 warnings & missing files

---
+++ msiupdate/headers.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <windows.h>
+#include <msi.h>
+#include <winhttp.h>
+
+#include <process.h>
+#include <time.h>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+#include "plugin.hpp"
