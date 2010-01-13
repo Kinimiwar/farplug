10a295a841e82b2a34113dd96a44db1439a10b9c
farplug <farplug@localhost>
2010-01-13 03:28:16 +0000

    ??????????? ?????? Update

--- update.cpp
+++ update.cpp
@@ -11,8 +11,11 @@
 #include "transact.hpp"
 #include "update.hpp"
 
-namespace Updater {
+namespace Update {
 
+const wchar_t* c_param_last_check_time = L"last_check_time";
+const wchar_t* c_param_last_check_version = L"last_check_version";
+const wchar_t* c_param_update_version = L"update_version";
 #ifdef _M_IX86
 const char* c_upgrade_code = "{67A59013-488F-4388-81F3-828A1DCEDA32}";
 const char* c_platform = "x86";
@@ -27,20 +30,23 @@ const wchar_t* changelog_url = L"http://farmanager.com/svn/trunk/unicode_far/cha
 const unsigned c_exit_wait = 6;
 const unsigned c_update_period = 12 * 60 * 60;
 
-HANDLE h_abort = NULL;
-HANDLE h_update_thread = NULL;
-unsigned curr_ver, update_ver;
-wstring msi_name;
+unsigned current_version;
 
-unsigned __stdcall check_thread(void* param) {
-  try {
-    string update_info = load_url(get_update_url(), g_options.http, h_abort);
-    if (check(update_info) && (update_ver > g_options.last_check_version))
-      Far::call_user_apc(0);
-  }
-  catch (...) {
-  }
-  return 0;
+struct UpdateInfo {
+  unsigned version;
+  wstring package_name;
+};
+
+UpdateInfo parse_update_info(const string& text) {
+  Ini::File update_ini;
+  update_ini.parse(text);
+  string ver_major = update_ini.get("far", "major");
+  string ver_minor = update_ini.get("far", "minor");
+  string ver_build = update_ini.get("far", "build");
+  UpdateInfo update_info;
+  update_info.version = MAKE_VERSION(str_to_int(ver_major), str_to_int(ver_minor), str_to_int(ver_build));
+  update_info.package_name = widen(update_ini.get("far", "msi"));
+  return update_info;
 }
 
 void check_product_installed() {
@@ -49,61 +55,13 @@ void check_product_installed() {
     FAIL_MSG(Far::get_msg(MSG_ERROR_NO_MSI));
 }
 
-void initialize() {
-  g_options.load();
-  curr_ver = Far::get_version();
-  h_abort = CreateEvent(NULL, TRUE, FALSE, NULL);
-  CHECK_SYS(h_abort);
-  check_product_installed();
-  time_t curr_time = time(NULL);
-  if (curr_time == -1)
-    FAIL(E_FAIL);
-  curr_time /= c_update_period;
-  if (curr_time == g_options.last_check_time)
-    return;
-  g_options.last_check_time = static_cast<unsigned>(curr_time);
-  g_options.save();
-  unsigned th_id;
-  h_update_thread = reinterpret_cast<HANDLE>(_beginthreadex(NULL, 0, check_thread, NULL, 0, &th_id));
-  CHECK_SYS(h_update_thread);
-}
-
-void finalize() {
-  if (h_update_thread) {
-    SetEvent(h_abort);
-    DWORD wait_res = WaitForSingleObject(h_update_thread, c_exit_wait * 1000);
-    if (wait_res != WAIT_OBJECT_0) {
-      TerminateThread(h_update_thread, 0);
-    }
-    CloseHandle(h_update_thread);
-  }
-  if (h_abort)
-    CloseHandle(h_abort);
-}
-
-wstring get_base_update_url() {
+wstring get_update_url() {
   wstring update_url = L"http://www.farmanager.com/";
   if (g_options.update_stable_builds)
     update_url += L"files/";
   else
     update_url += L"nightly/";
   return update_url;
-}
-
-wstring get_update_url() {
-  return get_base_update_url() + c_update_script;
-}
-
-bool check(const string& update_info) {
-  check_product_installed();
-  Ini::File update_ini;
-  update_ini.parse(update_info);
-  string ver_major = update_ini.get("far", "major");
-  string ver_minor = update_ini.get("far", "minor");
-  string ver_build = update_ini.get("far", "build");
-  update_ver = MAKE_VERSION(str_to_int(ver_major), str_to_int(ver_minor), str_to_int(ver_build));
-  msi_name = widen(update_ini.get("far", "msi"));
-  return update_ver > curr_ver;
 }
 
 void show_changelog(unsigned build1, unsigned build2) {
@@ -135,9 +93,9 @@ void show_changelog(unsigned build1, unsigned build2) {
 }
 
 enum UpdateDialogResult {
-  yes,
-  no,
-  cancel,
+  udrYes,
+  udrNo,
+  udrCancel,
 };
 
 class UpdateDialog: public Far::Dialog {
@@ -145,6 +103,8 @@ private:
   enum {
     c_client_xs = 40
   };
+
+  const UpdateInfo& update_info;
 
   int changelog_ctrl_id;
   int yes_ctrl_id;
@@ -157,18 +117,18 @@ private:
       return TRUE;
     }
     else if ((msg == DN_BTNCLICK) && (param1 == changelog_ctrl_id)) {
-      show_changelog(VER_BUILD(curr_ver), VER_BUILD(update_ver));
+      show_changelog(VER_BUILD(current_version), VER_BUILD(update_info.version));
     }
     return default_dialog_proc(msg, param1, param2);
   }
 
 public:
-  UpdateDialog(): Far::Dialog(Far::get_msg(MSG_PLUGIN_NAME), c_client_xs) {
+  UpdateDialog(const UpdateInfo& update_info): Far::Dialog(Far::get_msg(MSG_PLUGIN_NAME), c_client_xs), update_info(update_info) {
   }
 
   UpdateDialogResult show() {
     wostringstream st;
-    st << Far::get_msg(MSG_UPDATE_NEW_VERSION) << L' ' << VER_MAJOR(update_ver) << L'.' << VER_MINOR(update_ver) << L'.' << VER_BUILD(update_ver);
+    st << Far::get_msg(MSG_UPDATE_NEW_VERSION) << L' ' << VER_MAJOR(update_info.version) << L'.' << VER_MINOR(update_info.version) << L'.' << VER_BUILD(update_info.version);
     label(st.str());
     new_line();
     label(Far::get_msg(MSG_UPDATE_QUESTION));
@@ -183,9 +143,9 @@ public:
     new_line();
 
     int item = Far::Dialog::show();
-    if (item == yes_ctrl_id) return yes;
-    else if (item == no_ctrl_id) return no;
-    else return cancel;
+    if (item == yes_ctrl_id) return udrYes;
+    else if (item == no_ctrl_id) return udrNo;
+    else return udrCancel;
   }
 };
 
@@ -223,8 +183,15 @@ void save_to_cache(const string& package, const wstring& cache_dir, const wstrin
 }
 
 void execute() {
-  UpdateDialogResult res = UpdateDialog().show();
-  if (res == yes) {
+  check_product_installed();
+  string update_url_text = load_url(get_update_url() + c_update_script, g_options.http);
+  UpdateInfo update_info = parse_update_info(update_url_text);
+  if (update_info.version <= current_version) {
+    info_dlg(Far::get_msg(MSG_UPDATE_NO_NEW_VERSION));
+    return;
+  }
+  UpdateDialogResult res = UpdateDialog(update_info).show();
+  if (res == udrYes) {
     WindowInfo window_info;
     bool editor_unsaved = false;
     for (unsigned idx = 0; !editor_unsaved && Far::get_short_window_info(idx, window_info); idx++) {
@@ -237,8 +204,8 @@ void execute() {
     wstring cache_dir;
     if (g_options.cache_enabled) {
       cache_dir = expand_env_vars(g_options.cache_dir);
-      string package = load_url(get_base_update_url() + msi_name, g_options.http);
-      save_to_cache(package, cache_dir, msi_name);
+      string package = load_url(get_update_url() + update_info.package_name, g_options.http);
+      save_to_cache(package, cache_dir, update_info.package_name);
     }
 
     wostringstream st;
@@ -249,12 +216,12 @@ void execute() {
     if (g_options.logged_install) {
       wchar_t temp_path[MAX_PATH];
       CHECK_SYS(GetTempPathW(ARRAYSIZE(temp_path), temp_path));
-      st << L"/log \"" << add_trailing_slash(temp_path) << L"MsiUpdate_" << VER_MAJOR(curr_ver) << L"_" << widen(c_platform) << L".log\" ";
+      st << L"/log \"" << add_trailing_slash(temp_path) << L"MsiUpdate_" << VER_MAJOR(current_version) << L"_" << widen(c_platform) << L".log\" ";
     }
     if (g_options.cache_enabled)
-      st << "/i \"" << add_trailing_slash(cache_dir) + msi_name + L"\"";
+      st << "/i \"" << add_trailing_slash(cache_dir) + update_info.package_name + L"\"";
     else
-      st << "/i \"" << get_base_update_url() + msi_name + L"\"";
+      st << "/i \"" << get_update_url() + update_info.package_name + L"\"";
     if (!g_options.install_properties.empty())
       st << L" " << g_options.install_properties;
     wstring command = st.str();
@@ -273,9 +240,65 @@ void execute() {
       keys.push_back(KEY_ENTER);
     Far::post_keys(keys);
   }
-  else if (res == no) {
-    g_options.last_check_version = update_ver;
-    g_options.save();
+  else if (res == udrNo) {
+    Options::set_int(c_param_last_check_version, update_info.version);
+  }
+}
+
+HANDLE g_h_abort = NULL;
+HANDLE g_h_thread = NULL;
+wstring g_update_url;
+HttpOptions g_http_options;
+unsigned __stdcall check_thread(void* arg) {
+  try {
+    string update_url_text = load_url(g_update_url, g_http_options, g_h_abort);
+    UpdateInfo update_info = parse_update_info(update_url_text);
+    Options::set_int(c_param_update_version, update_info.version);
+    Far::call_user_apc(reinterpret_cast<void*>(cmdClean));
+  }
+  catch (...) {
+  }
+  return 0;
+}
+
+void init() {
+  current_version = Far::get_version();
+  check_product_installed();
+  unsigned update_version = Options::get_int(c_param_update_version);
+  unsigned last_check_version = Options::get_int(c_param_last_check_version);
+  if ((update_version > current_version) && (update_version > last_check_version)) {
+    Far::call_user_apc(reinterpret_cast<void*>(cmdExecute));
+    return;
+  }
+  time_t curr_time = time(NULL);
+  CHECK(curr_time != -1);
+  curr_time /= c_update_period;
+  unsigned last_check_time = Options::get_int(c_param_last_check_time);
+  if (curr_time == last_check_time)
+    return;
+  Options::set_int(c_param_last_check_time, static_cast<unsigned>(curr_time));
+  g_update_url = get_update_url() + c_update_script;
+  g_http_options = g_options.http;
+  g_h_abort = CreateEvent(NULL, TRUE, FALSE, NULL);
+  CHECK_SYS(g_h_abort);
+  unsigned th_id;
+  g_h_thread = reinterpret_cast<HANDLE>(_beginthreadex(NULL, 0, check_thread, NULL, 0, &th_id));
+  CHECK_SYS(g_h_thread);
+}
+
+void clean() {
+  if (g_h_thread) {
+    SetEvent(g_h_abort);
+    DWORD wait_res = WaitForSingleObject(g_h_thread, c_exit_wait * 1000);
+    if (wait_res != WAIT_OBJECT_0) {
+      TerminateThread(g_h_thread, 0);
+    }
+    CloseHandle(g_h_thread);
+    g_h_thread = NULL;
+  }
+  if (g_h_abort) {
+    CloseHandle(g_h_abort);
+    g_h_abort = NULL;
   }
 }
 
