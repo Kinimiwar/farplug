0835f21efab862bd4e25d78e5f39b67990534f4f
admin <admin@localhost>
2010-02-01 17:19:56 +0200

    ??????????? ???????????????? ???? ??????? ? ProgressMonitor (?? ????? ?????? ??????? ? ?.?.)

--- ui.cpp
+++ ui.cpp
@@ -5,27 +5,38 @@
 #include "sysutils.hpp"
 #include "ui.hpp"
 
+  unsigned __int64 cnt_prev;
+  unsigned __int64 cnt_freq;
+  unsigned __int64 time_total;
+  unsigned __int64 time_update;
+
+
 ProgressMonitor::ProgressMonitor(bool lazy): h_scr(NULL) {
-  QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&t_curr));
-  t_start = t_curr;
-  QueryPerformanceFrequency(reinterpret_cast<PLARGE_INTEGER>(&t_freq));
+  QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&time_cnt));
+  QueryPerformanceFrequency(reinterpret_cast<PLARGE_INTEGER>(&time_freq));
+  time_total = 0;
   if (lazy)
-    t_next = t_curr + t_freq / 2;
+    time_update = time_total + time_freq / c_first_delay_div;
   else
-    t_next = t_curr;
+    time_update = time_total;
 }
 
 ProgressMonitor::~ProgressMonitor() {
   if (h_scr) {
     Far::restore_screen(h_scr);
+    Far::flush_screen();
     SetConsoleTitleW(con_title.data());
     Far::set_progress_state(TBPF_NOPROGRESS);
   }
 }
 
 void ProgressMonitor::update_ui(bool force) {
-  QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&t_curr));
-  if ((t_curr >= t_next) || force) {
+  unsigned __int64 time_curr;
+  QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&time_curr));
+  time_total += time_curr - time_cnt;
+  time_cnt = time_curr;
+  if ((time_total >= time_update) || force) {
+    time_update = time_total + time_freq / c_update_delay_div;
     if (h_scr == NULL) {
       Far::flush_screen();
       h_scr = Far::save_screen();
@@ -40,9 +51,16 @@ void ProgressMonitor::update_ui(bool force) {
       ReadConsoleInput(h_con, &rec, 1, &read_cnt);
       if ((rec.EventType == KEY_EVENT) && (rec.Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE) && rec.Event.KeyEvent.bKeyDown && ((rec.Event.KeyEvent.dwControlKeyState & (LEFT_ALT_PRESSED | LEFT_CTRL_PRESSED | RIGHT_ALT_PRESSED | RIGHT_CTRL_PRESSED | SHIFT_PRESSED)) == 0)) FAIL(E_ABORT);
     }
-    t_next = t_curr + t_freq / 2;
     do_update_ui();
   }
+}
+
+ProgressSuspend::ProgressSuspend(ProgressMonitor& pm): pm(pm) {
+  pm.update_ui();
+}
+
+ProgressSuspend::~ProgressSuspend() {
+  QueryPerformanceCounter(reinterpret_cast<PLARGE_INTEGER>(&pm.time_cnt));
 }
 
 const wchar_t** get_size_suffixes() {
--- ui.hpp
+++ ui.hpp
@@ -1,22 +1,33 @@
 #pragma once
 
-class ProgressMonitor {
+class ProgressMonitor: private NonCopyable {
 private:
   HANDLE h_scr;
   wstring con_title;
-  unsigned __int64 t_start;
-  unsigned __int64 t_curr;
-  unsigned __int64 t_next;
-  unsigned __int64 t_freq;
+  static const unsigned c_first_delay_div = 2;
+  static const unsigned c_update_delay_div = 2;
+  unsigned __int64 time_cnt;
+  unsigned __int64 time_freq;
+  unsigned __int64 time_total;
+  unsigned __int64 time_update;
 protected:
   unsigned __int64 time_elapsed() const {
-    return (t_curr - t_start) / t_freq;
+    return time_total / time_freq;
   }
   virtual void do_update_ui() = 0;
 public:
   ProgressMonitor(bool lazy = true);
   virtual ~ProgressMonitor();
   void update_ui(bool force = false);
+  friend class ProgressSuspend;
+};
+
+class ProgressSuspend: private NonCopyable {
+private:
+  ProgressMonitor& pm;
+public:
+  ProgressSuspend(ProgressMonitor& pm);
+  ~ProgressSuspend();
 };
 
 const wchar_t** get_size_suffixes();
