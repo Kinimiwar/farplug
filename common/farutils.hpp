8185d1fc4fae32cdf69616601c9404584a3743a2
admin <admin@localhost>
2010-03-16 14:54:26 +0200

    ???????? ??????? ????????? ????? common ??? ???????

---
+++ common/farutils.hpp
@@ -0,0 +1,169 @@
+#pragma once
+
+extern wstring get_error_dlg_title();
+
+#define FAR_ERROR_HANDLER_BEGIN \
+  try { \
+    try {
+
+#define FAR_ERROR_HANDLER_END(return_error, return_cancel, silent) \
+    } \
+    catch (const Error& e) { \
+      if (e.code == E_ABORT) { \
+        return_cancel; \
+      } \
+      else { \
+        if (!silent) \
+          Far::error_dlg(get_error_dlg_title(), e); \
+        return_error; \
+      } \
+    } \
+    catch (const std::exception& e) { \
+      if (!silent) \
+        Far::error_dlg(get_error_dlg_title(), e); \
+      return_error; \
+    } \
+  } \
+  catch (...) { \
+    return_error; \
+  }
+
+namespace Far {
+
+void init(const PluginStartupInfo* psi);
+wstring get_plugin_module_path();
+wstring get_root_key_name();
+
+#define MAKE_VERSION(major, minor, build) (((major) << 24) | ((minor) << 16) | (build))
+#define VER_MAJOR(version) HIBYTE(HIWORD(version))
+#define VER_MINOR(version) LOBYTE(HIWORD(version))
+#define VER_BUILD(version) LOWORD(version)
+unsigned get_version();
+
+const wchar_t* msg_ptr(int id);
+wstring get_msg(int id);
+
+unsigned get_optimal_msg_width();
+int message(const wstring& msg, int button_cnt, DWORD flags);
+int menu(const wstring& title, const vector<wstring>& items, const wchar_t* help = NULL);
+
+wstring get_progress_bar_str(unsigned width, unsigned __int64 completed, unsigned __int64 total);
+void set_progress_state(TBPFLAG state);
+void set_progress_value(unsigned __int64 completed, unsigned __int64 total);
+
+void call_user_apc(void* param);
+void quit();
+
+HANDLE save_screen();
+void restore_screen(HANDLE h_scr);
+void flush_screen();
+
+int viewer(const wstring& file_name, const wstring& title);
+
+int update_panel(HANDLE h_plugin, bool keep_selection);
+
+void error_dlg(const wstring& title, const Error& e);
+void info_dlg(const wstring& title, const wstring& msg);
+
+#define AUTO_SIZE (-1)
+const unsigned c_x_frame = 5;
+const unsigned c_y_frame = 2;
+
+struct DialogItem {
+  DialogItemTypes type;
+  unsigned x1;
+  unsigned y1;
+  unsigned x2;
+  unsigned y2;
+  DWORD flags;
+  bool focus;
+  bool default_button;
+  int selected;
+  unsigned history_idx;
+  unsigned mask_idx;
+  unsigned text_idx;
+  unsigned list_idx;
+  unsigned list_size;
+  unsigned list_pos;
+  DialogItem() {
+    memset(this, 0, sizeof(*this));
+  }
+};
+
+class Dialog {
+private:
+  unsigned client_xs;
+  unsigned client_ys;
+  unsigned x;
+  unsigned y;
+  const wchar_t* help;
+  vector<wstring> values;
+  vector<DialogItem> items;
+  HANDLE h_dlg;
+  unsigned new_value(const wstring& text);
+  const wchar_t* get_value(unsigned idx) const;
+  void frame(const wstring& text);
+  void calc_frame_size();
+  unsigned new_item(const DialogItem& di);
+  static LONG_PTR WINAPI internal_dialog_proc(HANDLE h_dlg, int msg, int param1, LONG_PTR param2);
+protected:
+  virtual LONG_PTR default_dialog_proc(int msg, int param1, LONG_PTR param2);
+  virtual LONG_PTR dialog_proc(int msg, int param1, LONG_PTR param2) {
+    return default_dialog_proc(msg, param1, param2);
+  }
+public:
+  Dialog(const wstring& title, unsigned width, const wchar_t* help = NULL);
+  // create different controls
+  void new_line();
+  void spacer(unsigned size);
+  void pad(unsigned pos) {
+    if (pos > x - c_x_frame) spacer(pos - (x - c_x_frame));
+  }
+  unsigned separator();
+  unsigned label(const wstring& text, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  unsigned edit_box(const wstring& text, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  unsigned mask_edit_box(const wstring& text, const wstring& mask, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  unsigned fix_edit_box(const wstring& text, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  unsigned pwd_edit_box(const wstring& text, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  unsigned button(const wstring& text, DWORD flags = 0, bool def = false);
+  unsigned def_button(const wstring& text, DWORD flags = 0) {
+    return button(text, flags, true);
+  }
+  unsigned check_box(const wstring& text, int value, DWORD flags = 0);
+  unsigned check_box(const wstring& text, bool value, DWORD flags = 0) {
+    return check_box(text, value ? 1 : 0, flags);
+  }
+  unsigned radio_button(const wstring& text, bool value, DWORD flags = 0);
+  unsigned combo_box(const vector<wstring>& items, unsigned sel_idx, unsigned boxsize = AUTO_SIZE, DWORD flags = 0);
+  // display dialog
+  int show();
+  // utilities to set/get control values
+  wstring get_text(unsigned ctrl_id);
+  void set_text(unsigned ctrl_id, const wstring& text);
+  bool get_check(unsigned ctrl_id);
+  void set_check(unsigned ctrl_id, bool check);
+  unsigned get_list_pos(unsigned ctrl_id);
+  void set_color(unsigned ctrl_id, unsigned char color);
+  void set_focus(unsigned ctrl_id);
+  void enable(unsigned ctrl_id, bool enable);
+};
+
+class Regex: private NonCopyable {
+private:
+  HANDLE h_regex;
+public:
+  Regex();
+  ~Regex();
+  size_t search(const wstring& expr, const wstring& text);
+};
+
+class Selection {
+private:  
+  HANDLE h_plugin;
+public:  
+  Selection(HANDLE h_plugin);
+  ~Selection();
+  void select(unsigned idx, bool value);
+};
+
+};
---
+++ common/sysutils.hpp
@@ -0,0 +1,166 @@
+#pragma once
+
+#ifdef DEBUG
+  #define DEBUG_OUTPUT(msg) OutputDebugStringW(((msg) + L"\n").c_str())
+#else
+  #define DEBUG_OUTPUT(msg)
+#endif
+
+extern HINSTANCE g_h_instance;
+
+wstring get_system_message(HRESULT hr);
+wstring get_console_title();
+bool wait_for_single_object(HANDLE handle, DWORD timeout);
+wstring ansi_to_unicode(const string& str, unsigned code_page);
+string unicode_to_ansi(const wstring& str, unsigned code_page);
+wstring expand_env_vars(const wstring& str);
+wstring get_full_path_name(const wstring& path);
+wstring get_current_directory();
+
+class CriticalSection: private NonCopyable, private CRITICAL_SECTION {
+public:
+  CriticalSection() {
+    InitializeCriticalSection(this);
+  }
+  virtual ~CriticalSection() {
+    DeleteCriticalSection(this);
+  }
+  friend class CriticalSectionLock;
+};
+
+class CriticalSectionLock: private NonCopyable {
+private:
+  CriticalSection& cs;
+public:
+  CriticalSectionLock(CriticalSection& cs): cs(cs) {
+    EnterCriticalSection(&cs);
+  }
+  ~CriticalSectionLock() {
+    LeaveCriticalSection(&cs);
+  }
+};
+
+class File: private NonCopyable {
+protected:
+  HANDLE h_file;
+public:
+  File(const wstring& file_path, DWORD dwDesiredAccess, DWORD dwShareMode, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes);
+  File(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTransaction);
+  ~File();
+  unsigned __int64 size();
+  unsigned read(Buffer<char>& buffer);
+  void write(const void* data, unsigned size);
+  void set_time(const FILETIME* ctime, const FILETIME* atime, const FILETIME* mtime);
+};
+
+class Key: private NonCopyable {
+private:
+  HKEY h_key;
+public:
+  Key(HKEY hKey, LPCWSTR lpSubKey, REGSAM samDesired);
+  Key(HKEY hKey, LPCWSTR lpSubKey, REGSAM samDesired, HANDLE hTransaction);
+  ~Key();
+  bool query_bool(const wchar_t* name, bool def_value = false);
+  unsigned query_int(const wchar_t* name, unsigned def_value = 0);
+  wstring query_str(const wchar_t* name, const wstring& def_value = wstring());
+  void set_bool(const wchar_t* name, bool value);
+  void set_int(const wchar_t* name, unsigned value);
+  void set_str(const wchar_t* name, const wstring& value);
+  void delete_value(const wchar_t* name);
+};
+
+class FileEnum: private NonCopyable {
+protected:
+  wstring dir_path;
+  HANDLE h_find;
+  WIN32_FIND_DATAW find_data;
+public:
+  FileEnum(const wstring& dir_path);
+  ~FileEnum();
+  bool next();
+  const WIN32_FIND_DATAW& data() const {
+    return find_data;
+  }
+};
+
+struct FindData: public WIN32_FIND_DATAW {
+  bool is_dir() const {
+    return (dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
+  }
+  unsigned __int64 size() const {
+    return (static_cast<unsigned __int64>(nFileSizeHigh) << 32) | nFileSizeLow;
+  }
+};
+
+FindData get_find_data(const wstring& path);
+
+class TempFile: private NonCopyable {
+private:
+  wstring path;
+public:
+  TempFile();
+  ~TempFile();
+  wstring get_path() const {
+    return path;
+  }
+};
+
+class Thread: private NonCopyable {
+private:
+  Error error;
+  static unsigned __stdcall thread_proc(void* arg);
+protected:
+  HANDLE h_thread;
+  virtual void run() = 0;
+public:
+  Thread();
+  virtual ~Thread();
+  void start();
+  bool wait(unsigned wait_time);
+  bool get_result();
+  Error get_error() {
+    return error;
+  }
+};
+
+class Event: private NonCopyable {
+protected:
+  HANDLE h_event;
+public:
+  Event(bool manual_reset, bool initial_state);
+  ~Event();
+  void set();
+};
+
+typedef LRESULT (CALLBACK *WindowProc)(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
+
+class WindowClass: private NonCopyable {
+protected:
+  wstring name;
+public:
+  WindowClass(const wstring& name, WindowProc window_proc);
+  virtual ~WindowClass();
+};
+
+class MessageWindow: public WindowClass {
+private:
+  static LRESULT CALLBACK message_window_proc(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
+protected:
+  HWND h_wnd;
+  virtual LRESULT window_proc(UINT msg, WPARAM w_param, LPARAM l_param) = 0;
+  void end_message_loop(unsigned result);
+public:
+  MessageWindow(const wstring& name);
+  virtual ~MessageWindow();
+  unsigned message_loop(HANDLE h_abort);
+};
+
+class Icon: private NonCopyable {
+protected:
+  HICON h_icon;
+public:
+  Icon(HMODULE h_module, WORD icon_id, int width, int height);
+  ~Icon();
+};
+
+wstring format_file_time(const FILETIME& file_time);
