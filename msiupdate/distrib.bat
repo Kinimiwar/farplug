eb63870cbe5915e3a9b72acb2acc24d0da700113
farplug <farplug@localhost>
2010-07-31 11:54:44 +0300

    ???????????? distrib.bat

--- msiupdate/distrib.bat
+++ msiupdate/distrib.bat
@@ -1,20 +1,20 @@
-@call setenv /x86
+@call "%VCINSTALLDIR%\vcvarsall.bat" x86
 
-nmake -nologo release=1 clean
-nmake -nologo release=1 distrib installer
+nmake -nologo RELEASE=1 clean
+nmake -nologo RELEASE=1 distrib installer
 @if errorlevel 1 goto error
 @copy Release.x86\*.7z .
 @copy Release.x86\*.msi .
-nmake -nologo release=1 clean
+nmake -nologo RELEASE=1 clean
 
-@call setenv /x64
+@call "%VCINSTALLDIR%\vcvarsall.bat" x86_amd64
 
-nmake -nologo release=1 clean
-nmake -nologo release=1 distrib installer
+nmake -nologo RELEASE=1 PLATFORM=x64 clean
+nmake -nologo RELEASE=1 PLATFORM=x64 distrib installer
 @if errorlevel 1 goto error
 @copy Release.x64\*.7z .
 @copy Release.x64\*.msi .
-nmake -nologo release=1 clean
+nmake -nologo RELEASE=1 PLATFORM=x64 clean
 
 @goto end
 
