cc3aa6c1e80e942abc56d9113e4ed39486ca4be5
farplug <farplug@localhost>
2010-10-03 14:05:50 +0300

    ????????? ? ????????? ???? ??? ????????? ?????????

--- arclite/update.cpp
+++ arclite/update.cpp
@@ -72,7 +72,7 @@ private:
     Far::set_progress_state(TBPF_NORMAL);
     Far::set_progress_value(percent, 100);
 
-    SetConsoleTitleW((L"{" + int_to_str(percent) + L"%} " + Far::get_msg(new_arc ? MSG_PROGRESS_CREATE : MSG_PROGRESS_UPDATE)).c_str());
+    SetConsoleTitleW((L"{" + int_to_str(percent) + L"%} " + Far::get_msg(paused ? MSG_PROGRESS_PAUSED : new_arc ? MSG_PROGRESS_CREATE : MSG_PROGRESS_UPDATE)).c_str());
   }
 
   virtual void do_process_key(const KEY_EVENT_RECORD& key_event) {
